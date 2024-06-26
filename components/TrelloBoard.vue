7796fffbe09a13785b2553ad8a1ee69765006429
BenjCH <benjamin.choron@outlook.com>
2024-06-26 15:36:52 +0200

    ✨ Can delete task

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -59,7 +59,14 @@ const alt = useKeyModifier("Alt");
           >
             <template #item="{ element: task }: { element: Task }">
               <div>
-                <TrelloBoardTask :task="task" />
+                <TrelloBoardTask
+                  :task="task"
+                  @delete="
+                    column.tasks = column.tasks.filter(
+                      (task) => task.id !== $event
+                    )
+                  "
+                />
               </div>
             </template>
           </draggable>
--- components/TrelloBoardTask.vue
+++ components/TrelloBoardTask.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
-import type { Task } from "../types";
+import type { Task, ID } from "../types";
 
-defineProps<{ task: Task }>();
+const props = defineProps<{ task: Task }>();
+
+const emit = defineEmits<{
+  (e: "delete", payload: ID): void;
+}>();
+
+const focused = ref(false);
+onKeyStroke("Backspace", () => {
+  if (focused.value) emit("delete", props.task.id);
+});
 </script>
 
 <template>
   <div
     :title="task.createdAt.toLocaleDateString()"
     class="task bg-white p-2 mb-2 rounded shadow-sm max-w-[250px] flex"
+    @focus="focused = true"
+    @blur="focused = false"
+    tabindex="0"
   >
     <DragHandle class="pr-2" />
     <span>{{ task.title }}</span>
@@ -18,11 +30,19 @@ defineProps<{ task: Task }>();
 .sortable-drag .task {
   transform: rotate(5deg);
 }
+
 .sortable-ghost .task {
   position: relative;
 }
+
 .sortable-ghost .task::after {
   content: "";
   @apply absolute inset-0 bg-slate-300 rounded;
+}
+
+.task:focus,
+.task:focus-visible {
+  @apply outline-gray-400 !important;
+  outline: gray auto 1px;
 }
 </style>
