7cf2369fb3c70560dafad23eaf89dfd332503e4d
BenjCH <benjamin.choron@outlook.com>
2024-06-26 15:41:30 +0200

    ✨ Can delete column and edit title

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -48,7 +48,17 @@ const alt = useKeyModifier("Alt");
         <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
           <header class="font-bold mb-4">
             <DragHandle />
-            {{ column.title }}
+            <input
+              class="title-input bg-transparent focus:bg-white rounded px-1 w-4/5"
+              @keyup.enter="($event.target as HTMLInputElement).blur()"
+              @keydown.backspace="
+                column.title === ''
+                  ? (columns = columns.filter((c) => c.id !== column.id))
+                  : null
+              "
+              type="text"
+              v-model="column.title"
+            />
           </header>
           <draggable
             v-model="column.tasks"
