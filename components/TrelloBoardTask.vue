808a4e7fcc04418d88c7d5ac40f90dc33eac3328
BenjCH <benjamin.choron@outlook.com>
2024-06-26 15:10:09 +0200

    💄 Styling drag action

--- components/TrelloBoardTask.vue
+++ components/TrelloBoardTask.vue
@@ -13,3 +13,16 @@ defineProps<{ task: Task }>();
     <span>{{ task.title }}</span>
   </div>
 </template>
+
+<style>
+.sortable-drag .task {
+  transform: rotate(5deg);
+}
+.sortable-ghost .task {
+  position: relative;
+}
+.sortable-ghost .task::after {
+  content: "";
+  @apply absolute inset-0 bg-slate-300 rounded;
+}
+</style>
